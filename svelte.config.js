269f6b87452bc6c51e9873d1b153c8da9f3c8507
Cameron Johnson <cameronjohnson@Camerons-MacBook-Air-2.local>
2024-07-31 14:01:15 -0600

    Changed to node

--- svelte.config.js
+++ svelte.config.js
@@ -4,16 +4,19 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  preprocess: sveltePreprocess({
-    postcss: true
-  }),
-  kit: {
-    adapter: adapter({
-      fallback: '404.html'
+  preprocess: [
+    sveltePreprocess({
+      postcss: true
     }),
-
+    vitePreprocess()
+  ],
+  kit: {
+    adapter: adapter(),
     paths: {
-      base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
+      base: process.env.NODE_ENV === 'development' ? '' : process.env.BASE_PATH || ''
+    },
+    prerender: {
+      entries: []
     }
   }
 };
