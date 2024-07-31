cceaa17fec132f49a73be5c17a55fde8372bf72d
Cameron Johnson <cameronjohnson@Camerons-MacBook-Air-2.local>
2024-07-31 14:31:09 -0600

    Changed to node

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,17 @@
-import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import { sveltekit } from '@sveltejs/kit/vite';
+import dotenv from 'dotenv';
+
+// Load environment variables from .env file
+dotenv.config();
 
 export default defineConfig({
-	plugins: [sveltekit()]
+  plugins: [sveltekit()],
+  define: {
+    'process.env': {
+      BASE_PATH: JSON.stringify(process.env.BASE_PATH),
+      SUPABASE_URL: JSON.stringify(process.env.SUPABASE_URL),
+      SUPABASE_ANON_KEY: JSON.stringify(process.env.SUPABASE_ANON_KEY),
+    }
+  }
 });
