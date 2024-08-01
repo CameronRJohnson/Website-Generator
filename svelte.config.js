487ed478cb608bd5319e61ed9e9ca02d7b85914a
Cameron Johnson <cameronjohnson@Camerons-MacBook-Air-2.local>
2024-08-01 15:57:01 -0600

    Build

--- svelte.config.js
+++ svelte.config.js
@@ -2,21 +2,24 @@ import adapter from '@sveltejs/adapter-static';
 import sveltePreprocess from 'svelte-preprocess';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
-const isGitHubPages = process.env.NODE_ENV === 'production' && process.env.BASE_PATH;
-
+/** @type {import('@sveltejs/kit').Config} */
 const config = {
   preprocess: sveltePreprocess({
     postcss: true
   }),
   kit: {
     adapter: adapter({
-      pages: 'build',
-      assets: 'build',
-      fallback: null,
+      fallback: '404.html'
     }),
+
     paths: {
-      base: isGitHubPages ? '/Website-Generator' : '',
+      base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
+    },
+    
+    prerender: {
+      entries: ['*', '/apple', '/banana', '/cherry']  // Add all the possible routes here
     }
+
   }
 };
 
